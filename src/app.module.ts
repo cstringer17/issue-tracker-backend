629585fd526b36183b7d398c55e904479a9378c9
callum stringer <callumstringer10@gmail.com>
2021-04-19 15:17:21 +0100

    super duper basic auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule} from "@nestjs/common";
+import { Controller, MiddlewareConsumer, Module, NestModule, Post, UseGuards, Request } from "@nestjs/common";
 
 import { logger} from "./logger.middleware";
 import { UsersModule } from './users/users.module';
@@ -6,13 +6,30 @@ import { IssuesModule } from './issues/issues.module';
 import { IssuesController } from "./issues/issues.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { configService } from "./config/config.service";
+import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from "@nestjs/passport";
+
+
+@Controller()
+export class AuthController {
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
 
 @Module({
   imports: [
+    AuthModule,
     IssuesModule,
     UsersModule,
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    AuthModule
   ],
+  controllers:[
+    AuthController
+  ]
 })
 export class AppModule implements NestModule{
   configure(consumer: MiddlewareConsumer): any {
@@ -21,3 +38,5 @@ export class AppModule implements NestModule{
       .forRoutes(IssuesController);
   }
 }
+
+
